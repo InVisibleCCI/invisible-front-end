c38a497412d3fa34ba7e997d6118b61f3068d891
Quentin <quentin@alterealities.com>
2022-03-22 09:45:56 +0100

    [IN-00]: Styles

--- src/utils/styles.ts
+++ src/utils/styles.ts
@@ -1,10 +1,20 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const colors = {
+  textColor: '#000000',
+  pink: '#FF7E7E',
+  flirt: '#A02074',
+  magenta: '#CF0063',
+  blue: '#2F789D',
+  lightBlue: '#06C7F2',
+  darkBlue: '#5B53AE',
+  purple: '#7F20A0',
+  green: '#20A091',
+  orange: '#EF9935',
 }
 
 export const GlobalStyles = createGlobalStyle`
-  html, body {
+  html, body #root {
     width: 100%;
     height: 100%;
   }
@@ -12,5 +22,11 @@ export const GlobalStyles = createGlobalStyle`
   body {
     margin: 0;
     font-family: 'Roboto';
+    font-size: 16px;
+    color: ${colors.textColor};
+
+    h1, h2, h3, h4, h5, h6 {
+      font-family: 'Montserrat Alternates';
+    }
   }
 `
