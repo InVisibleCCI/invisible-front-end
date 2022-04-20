b2c4f9e51d613070596114b5d24291c09249ae7f
Aranxa Codina <a.codina@sfereno.com>
2022-04-20 19:23:15 +0200

    IN-25 : clean-up :)

--- src/components/EventCard/Difficulty/index.tsx
+++ src/components/EventCard/Difficulty/index.tsx
@@ -1,8 +1,7 @@
-import { Props } from "react"
-import React, { CSSProperties } from "react"
+import React from "react"
 
 export interface DifficultyProps {
-  difficulty?: string,
+    difficulty?: number,
 }
 
 /** Difficulty icon for an activity
@@ -10,51 +9,50 @@ export interface DifficultyProps {
  * @param props only using props.difficulty [string] to determine wich svg to display
  * @constructor
  */
-const Difficulty: React.FunctionComponent<DifficultyProps> = (props) => {
+const Difficulty: React.FunctionComponent<DifficultyProps> = ({ difficulty }) => {
 
-    const difficulty = props.difficulty
 
-    const styles = {
-        difficulty: {
+    return <React.Fragment>
+        {
+            difficulty == null && <span>
+                <svg width="22" height="25" viewBox="0 0 22 25" fill="none" xmlns="http:www.w3.org/2000/svg">
+                    <rect x="0.5" y="15.5" width="5" height="9" rx="1.5" stroke="white" />
+                    <rect x="8.5" y="8.5" width="5" height="16" rx="1.5" stroke="white" />
+                    <rect x="16.5" y="0.5" width="5" height="24" rx="1.5" stroke="white" />
+                </svg>
+            </span>
 
-        },
-    }
+        }
+        {
+            difficulty == 1 && <span>
+                <svg width="22" height="25" viewBox="0 0 22 25" fill="none" xmlns="http:www.w3.org/2000/svg">
+                    <rect x="0.5" y="15.5" width="5" height="9" rx="1.5" fill="white" />
+                    <rect x="8.5" y="8.5" width="5" height="16" rx="1.5" stroke="white" />
+                    <rect x="16.5" y="0.5" width="5" height="24" rx="1.5" stroke="white" />
+                </svg>
+            </span>
+        }
+        {
+            difficulty == 2 && <span>
+                <svg width="22" height="25" viewBox="0 0 22 25" fill="none" xmlns="http:www.w3.org/2000/svg">
+                    <rect x="0.5" y="15.5" width="5" height="9" rx="1.5" fill="white" />
+                    <rect x="8.5" y="8.5" width="5" height="16" rx="1.5" fill="white" stroke="white" />
+                    <rect x="16.5" y="0.5" width="5" height="24" rx="1.5" stroke="white" />
+                </svg>
+            </span>
+        }
+        {
+            difficulty == 3 && <span>
+                <svg width="22" height="25" viewBox="0 0 22 25" fill="none" xmlns="http:www.w3.org/2000/svg">
+                    <rect x="0.5" y="15.5" width="5" height="9" rx="1.5" fill="white" />
+                    <rect x="8.5" y="8.5" width="5" height="16" rx="1.5" fill="white" />
+                    <rect x="16.5" y="0.5" width="5" height="24" rx="1.5" fill="white" />
+                </svg>
+            </span>
 
-    if (difficulty == "0" || difficulty == "" || difficulty == null) {
-      return <span style={styles.difficulty}>
-            <svg width="22" height="25" viewBox="0 0 22 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <rect x="0.5" y="15.5" width="5" height="9" rx="1.5" stroke="white"/>
-                <rect x="8.5" y="8.5" width="5" height="16" rx="1.5" stroke="white"/>
-                <rect x="16.5" y="0.5" width="5" height="24" rx="1.5" stroke="white"/>
-            </svg>
-        </span>;
-    } else if (difficulty == "1") {
-        return <span style={styles.difficulty}>
-              <svg width="22" height="25" viewBox="0 0 22 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <rect x="0.5" y="15.5" width="5" height="9" rx="1.5" fill="white"/>
-                  <rect x="8.5" y="8.5" width="5" height="16" rx="1.5" stroke="white"/>
-                  <rect x="16.5" y="0.5" width="5" height="24" rx="1.5" stroke="white"/>
-              </svg>
-          </span>;
-    } else if (difficulty == "2") {
-        return <span style={styles.difficulty}>
-            <svg width="22" height="25" viewBox="0 0 22 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <rect x="0.5" y="15.5" width="5" height="9" rx="1.5" fill="white"/>
-                <rect x="8.5" y="8.5" width="5" height="16" rx="1.5" fill="white" stroke="white"/>
-                <rect x="16.5" y="0.5" width="5" height="24" rx="1.5" stroke="white"/>
-            </svg>
-        </span>;
-    } else if (difficulty == "3") {
-        return <span style={styles.difficulty}>
-            <svg width="22" height="25" viewBox="0 0 22 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <rect x="0.5" y="15.5" width="5" height="9" rx="1.5" fill="white"/>
-                <rect x="8.5" y="8.5" width="5" height="16" rx="1.5" fill="white" />
-                <rect x="16.5" y="0.5" width="5" height="24" rx="1.5" fill="white"/>
-            </svg>
-        </span>;
-    }
+        }
 
-    return <></>;
+    </React.Fragment>
 }
 
 export default Difficulty
--- src/components/EventCard/LikeButton/index.tsx
+++ src/components/EventCard/LikeButton/index.tsx
@@ -1,79 +1,25 @@
-import { Props } from "react"
-import React, { CSSProperties } from "react"
+import { LikeFilled, LikeOutlined } from "components/Svg/Like";
+import React, { useState } from "react";
 
 export interface LikeButtonProps {
-  isLiked?: boolean,
+    isLiked: boolean,
 }
 
 /** Like button for an activity
  *
- * @param props only using props.isLiked [bool] to determine wich state is the component and wich heart is displayed
+ * @param props
  * @constructor
  */
-const LikeButton: React.FunctionComponent<LikeButtonProps> = (props) => {
+export const LikeButton: React.FunctionComponent<LikeButtonProps> = (props) => {
 
-    let isLiked = props.isLiked
+    const [isLiked, setIsLiked] = useState<boolean>(props.isLiked);
 
-    let buttonLiked = <path d="M18.9979 2.50895C18.442 1.94039 17.7811 1.48903 17.0529 1.18082C16.3248 0.872609 15.5439 0.713627 14.7551 0.713013C13.263 0.71326 11.8254 1.28259 10.727 2.30826C9.62865 1.28241 8.19102 0.71306 6.69893 0.713013C5.90918 0.713848 5.12739 0.873316 4.39855 1.18224C3.66971 1.49116 3.00821 1.94344 2.45211 2.51305C0.080227 4.93255 0.0812349 8.71692 2.45413 11.1262L10.727 19.5294L18.9999 11.1262C21.3728 8.71692 21.3738 4.93255 18.9979 2.50895Z" fill="white"/>;
-    let buttonNotLiked = <path d="M18.9979 2.50895C18.442 1.94039 17.7811 1.48903 17.0529 1.18082C16.3248 0.872609 15.5439 0.713627 14.7551 0.713013C13.263 0.71326 11.8254 1.28259 10.727 2.30826C9.62865 1.28241 8.19102 0.71306 6.69893 0.713013C5.90918 0.713848 5.12739 0.873316 4.39855 1.18224C3.66971 1.49116 3.00821 1.94344 2.45211 2.51305C0.080227 4.93255 0.0812349 8.71692 2.45413 11.1262L10.727 19.5294L18.9999 11.1262C21.3728 8.71692 21.3738 4.93255 18.9979 2.50895Z" stroke="white"/>;
-
-    let style = {
-      displayed: {
-          display: "block",
-      },
-      notDisplayed: {
-          display: "none" ,
-      },
-    }
-
-    return <>
-        <svg className="liked" style={ isLiked ? style.displayed : style.notDisplayed} onClick={((e) => clicked(e))} width="21" height="20" viewBox="0 0 21 20" fill="none">
-            {buttonLiked}
-        </svg>
-        <svg className="not-liked" style={ isLiked ? style.notDisplayed : style.displayed} onClick={(e) => clicked(e)} width="21" height="20" viewBox="0 0 21 20" fill="none">
-            {buttonNotLiked}
-        </svg>
-    </>;
-
-    /** Function that change the liked state
-     *
-     * @param e event
-     */
-    function clicked(e) {
-        isLiked = !isLiked;
-
-        // TODO add here the API call to add the event to the liked list
-
-        changeIcon(e.target)
-        return;
-    }
-
-  /** Function that change the like buttons style to switch from liked to not liked
-   *
-   * @param element element from event target
-   */
-    function changeIcon(element) {
-        let neighborSvg;
-        let thisSvg;
-
-        // get the neighbourg SVG
-        if (element.parentElement.nodeName == "SPAN") {
-            neighborSvg = element.parentElement.getElementsByClassName(isLiked ? "liked" : "not-liked")[0];
-            thisSvg = element;
-        } else if (element.parentElement.nodeName == "svg"){
-            if (element.parentElement.parentElement.nodeName == "SPAN") {
-                thisSvg = element.parentElement;
-                if (thisSvg.className == "liked") {
-                    neighborSvg = element.parentElement.parentElement.parentElement.getElementsByClassName(isLiked ? "liked" : "not-liked")[1];
-                } else {
-                    neighborSvg = element.parentElement.parentElement.parentElement.getElementsByClassName(isLiked ? "liked" : "not-liked")[0];
-                }
-            } else {return;}
-        } else {return;}
-
-        thisSvg.style.display = "none";
-        neighborSvg.style.display = "block";
-    }
+    return <React.Fragment>
+        {
+            isLiked ? <LikeFilled onClick={e => setIsLiked(false)} /> :
+                <LikeOutlined onClick={e => setIsLiked(true)} />
+        }
+    </React.Fragment>;
 }
 
 export default LikeButton
