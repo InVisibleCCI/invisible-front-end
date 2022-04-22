bcbbd18581165d4491e14b21fbef59ed269557c4
Kawawara <Tberton1>
2022-04-22 07:06:05 +0200

    [IN-25] separating SVGs into services

---
+++ src/components/Svg/Difficulty.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+export const DifficultyNull = () => {
+
+    return <span>
+        <svg width="22" height="25" viewBox="0 0 22 25" fill="none" xmlns="http:www.w3.org/2000/svg">
+            <rect x="0.5" y="15.5" width="5" height="9" rx="1.5" stroke="white" />
+            <rect x="8.5" y="8.5" width="5" height="16" rx="1.5" stroke="white" />
+            <rect x="16.5" y="0.5" width="5" height="24" rx="1.5" stroke="white" />
+        </svg>
+    </span>;
+}
+
+export const DifficultyOne = () => {
+
+    return <span>
+        <svg width="22" height="25" viewBox="0 0 22 25" fill="none" xmlns="http:www.w3.org/2000/svg">
+            <rect x="0.5" y="15.5" width="5" height="9" rx="1.5" fill="white" />
+            <rect x="8.5" y="8.5" width="5" height="16" rx="1.5" stroke="white" />
+            <rect x="16.5" y="0.5" width="5" height="24" rx="1.5" stroke="white" />
+        </svg>
+    </span>;
+}
+
+export const DifficultyTwo = () => {
+
+    return <span>
+        <svg width="22" height="25" viewBox="0 0 22 25" fill="none" xmlns="http:www.w3.org/2000/svg">
+            <rect x="0.5" y="15.5" width="5" height="9" rx="1.5" fill="white" />
+            <rect x="8.5" y="8.5" width="5" height="16" rx="1.5" fill="white" stroke="white" />
+            <rect x="16.5" y="0.5" width="5" height="24" rx="1.5" stroke="white" />
+        </svg>
+    </span>;
+}
+
+export const DifficultyThree = () => {
+
+    return <span>
+        <svg width="22" height="25" viewBox="0 0 22 25" fill="none" xmlns="http:www.w3.org/2000/svg">
+            <rect x="0.5" y="15.5" width="5" height="9" rx="1.5" fill="white" />
+            <rect x="8.5" y="8.5" width="5" height="16" rx="1.5" fill="white" />
+            <rect x="16.5" y="0.5" width="5" height="24" rx="1.5" fill="white" />
+        </svg>
+    </span>;
+}
