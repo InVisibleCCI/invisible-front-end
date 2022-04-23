f2da38ffad63f8495689334bb920b8a540be9686
Kawawara <Tberton1>
2022-04-23 22:49:15 +0200

    [IN-25] Modifying search bar to match moke-up

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -5,48 +5,56 @@ import { InstantSearch, SearchBox } from 'react-instantsearch-dom';
 import { useNavigate } from 'react-router-dom';
 import { useSessionContext } from 'utils/types/contexts/SessionContext';
 import DesktopResults from "./components/DesktopResult";
+import { SearchBarWrapper } from "./styles";
+import { SearchIcon } from "../../components/Svg/Search";
 
-interface Props {
-}
 
 // This component display SearchPage it uses AlgoliaComponents
-const Search: React.FunctionComponent<Props> = () => {
+const Search = () => {
 
-  // searchClient defines client with env params
-  const searchClient = algoliasearch(`${process.env.REACT_APP_ALGOLIA_APP_ID}`, `${process.env.REACT_APP_ALGOLIA_API_KEY}`);
+    // searchClient defines client with env params
+    const searchClient = algoliasearch(`${process.env.REACT_APP_ALGOLIA_APP_ID}`, `${process.env.REACT_APP_ALGOLIA_API_KEY}`);
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const { lastSearch, setLastSearch } = useSessionContext();
-  const { t } = useTranslation();
+    const { lastSearch, setLastSearch } = useSessionContext();
+    const { t } = useTranslation();
 
-  // This method use lastSearch (retrieve in SessionContext) to navigate to search page with search string. 
-  // This method is called on "onChange" from Searchbox (algolia component)
+    // This method use lastSearch (retrieve in SessionContext) to navigate to search page with search string.
+    // This method is called on "onChange" from Searchbox (algolia component)
 
-  const launchSearch = (value) => {
-    setLastSearch(value)
-    if (!value) {
-      navigate(`/search?term=${lastSearch}`);
+    const launchSearch = (value) => {
+        setLastSearch(value)
+        if (!value) {
+            navigate(`/search?term=${lastSearch}`);
+        }
+        if (value.replace(/\s/g, '') === "") {
+            navigate(`/search`);
+        } else {
+            navigate(`/search?term=${value}`);
+        }
     }
-    if (value.replace(/\s/g, '') === "") {
-      navigate(`/search`);
-    } else {
-      navigate(`/search?term=${value}`);
-    }
-  }
-  // InstantSearch and Searchbox are AlgoliaComponents
-  return <InstantSearch searchClient={searchClient} indexName="dev">
-    <SearchBox
-      translations={{ placeholder: t('search.input.main') }}
-      searchAsYouType
-      onChange={(e) => {
-        e.preventDefault();
-        launchSearch(e.target.value);
-      }}
-      defaultRefinement={lastSearch && lastSearch}
-    />
-    <DesktopResults />
-  </InstantSearch>
+    // InstantSearch and Searchbox are AlgoliaComponents
+    return <InstantSearch searchClient={searchClient} indexName="dev">
+        <SearchBarWrapper>
+            <div>
+                <SearchBox
+                    className={"algolia-searchBox"}
+                    translations={{ placeholder: t('search.input.main') }}
+                    searchAsYouType
+                    onChange={(e) => {
+                      e.preventDefault();
+                      launchSearch(e.target.value);
+                    }}
+                    defaultRefinement={lastSearch && lastSearch}
+                    reset={""} />
+
+                <SearchIcon />
+            </div>
+        </SearchBarWrapper>
+
+        <DesktopResults />
+    </InstantSearch>
 }
 
 export default Search;
