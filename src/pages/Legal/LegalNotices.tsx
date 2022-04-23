e1e6da0379c76c21afb1245f2240a6925399fb2c
Aranxa Codina <a.codina@sfereno.com>
2022-04-23 17:17:47 +0200

    IN-62: RGPD Ok ?

---
+++ src/pages/Legal/LegalNotices.tsx
@@ -0,0 +1,31 @@
+import React from "react"
+
+export const LegalNotice = () => {
+    return <React.Fragment>
+        <h1>Mentions légales </h1>
+        <p> Conformément aux dispositions des articles 6-III et 19 de la Loi n° 2004-575 du 21 juin 2004 pour la Confiance dans l’économie numérique, dite L.C.E.N., nous portons à la connaissance des utilisateurs et visiteurs du site : <strong> in-visible.ovh</strong> les informations suivantes : </p>
+        <p> Important : Ce site est un projet de formation au titre RNCP Concepteur Développeur d'application</p>
+
+        <p> <b> ÉDITEUR </b> </p>
+
+        <p>Le site in-visible.ovh est la propriété exclusive de InVisble </p>
+        <p>Directeur de la  publication : InVisible </p>
+        <p>Contactez le responsable de la publication : aranxa.codina@aquarius.irish </p>
+
+        <p> <b> HÉBERGEMENT </b> </p>
+
+
+        <p> Le site est hébergé par <strong> OVH 2 rue Kellermann 59100 Roubaix-Tourcoing  </strong></p>
+
+        <p> <b>DESCRIPTION DES SERVICES FOURNIS </b></p>
+        <p>
+            Le site InVisible a pour objet de fournir des informations concernant l’ensemble des activités mises en avant par les commerçant de la plateforme. Le directeur de la publication ne mettant pas en avant des activités, il ne pourra être tenu responsable des omissions, des inexactitudes et des carences dans la mise à jour, qu’elles soient de son fait ou du fait des tiers partenaires qui lui fournissent ces informations. Tous les informations proposées sur le site InVisble sont données à titre indicatif, sont non exhaustives, et sont susceptibles d’évoluer. Elles sont données sous réserve de modifications ayant été apportées depuis leur mise en ligne.
+        </p>
+
+
+        <p><b>PROPRIÉTÉ INTELLECTUELLE ET CONTREFAÇONS </b> </p>
+        <p>
+            Le proprietaire du site est propriétaire des droits de propriété intellectuelle ou détient les droits d’usage sur tous les éléments accessibles sur le site, notamment les textes, images, graphismes, logo, icônes, sons, logiciels… Toute reproduction, représentation, modification, publication, adaptation totale ou partielle des éléments du site, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable à l’email : aranxa.codina@aquarius.irish. Toute exploitation non autorisée du site ou de l’un quelconque de ces éléments qu’il contient sera considérée comme constitutive d’une contrefaçon et poursuivie conformément aux dispositions des articles L.335-2 et suivants du Code de Propriété Intellectuelle.
+        </p>
+    </React.Fragment>
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -7,6 +7,8 @@ import Search from 'pages/Search';
 import React from 'react';
 import { Route, Routes as DOMRoutes } from 'react-router-dom';
 import Activity from 'pages/Activity';
+import { LegalNotice } from "pages/Legal/LegalNotices";
+import { PrivacyPolicy } from "pages/Legal/PrivacyPolicy";
 
 const Routes: React.FunctionComponent = () => {
 
@@ -16,6 +18,8 @@ const Routes: React.FunctionComponent = () => {
     <Route path="/connection" element={<Connection />}></Route>
     <Route path="/register" element={<Register />}></Route>
     <Route path="/search" element={<Search />}></Route>
+    <Route path="/privacy-policy" element={<PrivacyPolicy/>}></Route> 
+    <Route path="/legal-notices" element={<LegalNotice/>}></Route> 
     <Route path="/" element={<Main />}>
     </Route>
     <Route path="*" element={<NotFound />} />
