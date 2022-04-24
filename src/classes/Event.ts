b3e2c0db3d6135b6d1925524eca9f948219ae39a
Kawawara <Tberton1>
2022-04-24 15:58:48 +0200

    [IN-77] Adding typed classes for Merchant and addapting Event class  + activityCard using event datas

--- src/classes/Event.ts
+++ src/classes/Event.ts
@@ -3,15 +3,19 @@ import {Review} from "./Review";
 import {AccessibilityCategory} from "./AccessibilityCategory";
 import {Address} from "./Address";
 import {Category} from "./Category";
+import {Merchant} from "./Merchant";
 
 export class Event {
   accessibility_categories: AccessibilityCategory[];
+  description: string;
   address: Address;
   categories: Category[];
   difficult: number;
   name: string;
   objectID: string;
   images: Image[];
-  average_mar: number;
+  average_mark: number;
+  reviews_count: number;
   reviews?: Review[];
+  merchant: Merchant
 }
